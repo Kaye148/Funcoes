41152511183cebb04e121435305be3e260b4e3e9
Kayky Sampaio <kaykysampaio13@gmail.com>
2025-10-03 16:12:16 -0300

    Add files via upload

--- atividade3.py
+++ atividade3.py
@@ -2,4 +2,9 @@ import os
 os.system("cls")
 
 def tobuada(numero):
-    printnu
+    for i in range(1,11):
+        print(f"o produto entre {i} x {numero} = {i*numero}")
+
+numero = int(input("informe um número: "))
+
+tobuada(numero)
